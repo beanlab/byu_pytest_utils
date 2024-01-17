e3680af628b3e31454a6fb35b9decafb019efb24
Gordon Bean <gbean@cs.byu.edu>
2024-01-17 15:44:01 -0700

    update tests

--- tests/test_run_api.py
+++ tests/test_run_api.py
@@ -4,12 +4,8 @@ from byu_pytest_utils import run_exec, max_score, test_files
 def test_run_exec():
     stats = run_exec("python3", "script_for_dialog_passes.py", 'woot', 7,
                      expected_stdio=test_files / "test_dialog_should_pass.txt")
-    assert 'stdout-seven' in stats
-    assert stats['stdout-seven']['passed']
-    assert 'stdout-another-number' in stats
-    assert stats['stdout-another-number']['passed']
-    assert 'stdout-everything-else' in stats
-    assert stats['stdout-everything-else']['passed']
+    assert 'stdout' in stats
+    assert stats['stdout']['passed']
 
 
 def test_run_exec_with_failed_test():
@@ -17,12 +13,8 @@ def test_run_exec_with_failed_test():
         "python3", "script_for_dialog_fails.py", 'woot', 7, 'foobar',
         expected_stdio=test_files / "test_dialog_should_pass.txt"
     )
-    assert 'stdout-seven' in stats
-    assert stats['stdout-seven']['passed']
-    assert 'stdout-another-number' in stats
-    assert not stats['stdout-another-number']['passed']
-    assert 'stdout-everything-else' in stats
-    assert not stats['stdout-everything-else']['passed']
+    assert 'stdout' in stats
+    assert not stats['stdout']['passed']
 
 
 '''
